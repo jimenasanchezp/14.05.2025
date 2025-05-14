ee8ab99e6695fbf6879e65a3b5ea1320662554eb
jimes <jimes@JIMENASANCHEZ>
2025-05-14 08:03:54 -0600

    Agregar archivos de proyecto.

---
+++ 14.05.2025/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace _14._05._2025
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lstLista = new System.Windows.Forms.ListView();
+            this.txtLectura = new System.Windows.Forms.TextBox();
+            this.txtGuardar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSi = new System.Windows.Forms.Button();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(24, 12);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(81, 18);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "NOMBRE";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(24, 37);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(203, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // lstLista
+            // 
+            this.lstLista.HideSelection = false;
+            this.lstLista.Location = new System.Drawing.Point(24, 63);
+            this.lstLista.Name = "lstLista";
+            this.lstLista.Size = new System.Drawing.Size(203, 251);
+            this.lstLista.TabIndex = 3;
+            this.lstLista.UseCompatibleStateImageBehavior = false;
+            this.lstLista.DoubleClick += new System.EventHandler(this.lstLista_DoubleClick);
+            // 
+            // txtLectura
+            // 
+            this.txtLectura.Location = new System.Drawing.Point(240, 63);
+            this.txtLectura.Multiline = true;
+            this.txtLectura.Name = "txtLectura";
+            this.txtLectura.Size = new System.Drawing.Size(251, 251);
+            this.txtLectura.TabIndex = 4;
+            // 
+            // txtGuardar
+            // 
+            this.txtGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGuardar.Location = new System.Drawing.Point(240, 34);
+            this.txtGuardar.Name = "txtGuardar";
+            this.txtGuardar.Size = new System.Drawing.Size(75, 23);
+            this.txtGuardar.TabIndex = 5;
+            this.txtGuardar.Text = "Guardar";
+            this.txtGuardar.UseVisualStyleBackColor = true;
+            this.txtGuardar.Click += new System.EventHandler(this.txtGuardar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(507, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "¿quiere modiifcar?";
+            // 
+            // btnSi
+            // 
+            this.btnSi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSi.Location = new System.Drawing.Point(528, 99);
+            this.btnSi.Name = "btnSi";
+            this.btnSi.Size = new System.Drawing.Size(43, 23);
+            this.btnSi.TabIndex = 7;
+            this.btnSi.Text = "Si";
+            this.btnSi.UseVisualStyleBackColor = true;
+            this.btnSi.Click += new System.EventHandler(this.btnSi_Click);
+            // 
+            // btnNo
+            // 
+            this.btnNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNo.Location = new System.Drawing.Point(589, 99);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(43, 23);
+            this.btnNo.TabIndex = 8;
+            this.btnNo.Text = "No";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(682, 341);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.btnSi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGuardar);
+            this.Controls.Add(this.txtLectura);
+            this.Controls.Add(this.lstLista);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ListView lstLista;
+        private System.Windows.Forms.TextBox txtLectura;
+        private System.Windows.Forms.Button txtGuardar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSi;
+        private System.Windows.Forms.Button btnNo;
+    }
+}
+
---
+++ 14.05.2025/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _14._05._2025
+{
+    public partial class Form1: Form
+    {
+        string nombredearchivo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstLista.View = View.Details;
+            lstLista.LabelEdit = true;
+            lstLista.GridLines = true;
+            lstLista.FullRowSelect = true;
+            lstLista.Columns.Add("Nombre", -2, HorizontalAlignment.Left);
+
+            //Obtener la ruta donde se ejecuta el programa
+            string ruta = AppDomain.CurrentDomain.BaseDirectory;
+            DirectoryInfo dir = new DirectoryInfo(ruta);
+
+            foreach (var fil in dir.GetFiles("*.txt"))
+            {
+                lstLista.Items.Add(fil.Name);
+            }
+            //foreach (var fil in dir.GetFiles("*.txt"))
+            //{
+            //    MessageBox.Show(fil.Name);
+            //}
+            //foreach (var fil in dir.GetFiles("h?.*"))
+            //{
+            //    MessageBox.Show(fil.Name);
+            //}
+            //foreach (var fil in dir.GetDirectories())
+            //{
+            //    MessageBox.Show(fil.Name);
+            //}
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstLista_DoubleClick(object sender, EventArgs e)
+        {
+            //StreamReader archivo;
+            //string linea; 
+            //archivo = File.OpenText(lstLista.SelectedItems[0].Text);
+            //linea = archivo.ReadToEnd();
+            //lstVerArchivo.Items.Add(linea);
+
+            nombredearchivo = lstLista.SelectedItems[0].Text;
+            txtLectura.Clear();
+            StreamReader archivo;
+            archivo = File.OpenText(nombredearchivo);
+            while (!archivo.EndOfStream)
+                txtLectura.Text += archivo.ReadLine() + "\r\n";
+            archivo.Close();
+        }
+
+        private void txtGuardar_Click(object sender, EventArgs e)
+        {
+            StreamWriter fichero;
+            fichero = File.CreateText("Fichero1.txt");
+            fichero.Close();
+        }
+
+        private void btnSi_Click(object sender, EventArgs e)
+        {
+            txtLectura.Enabled = true;
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            txtLectura.Enabled = false;
+        }
+    }
+}
